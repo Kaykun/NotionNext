13ff78f1e298532989429f1100b874a42d7c16b1
胡凯 <46771180+Kaykun@users.noreply.github.com>
2024-06-07 16:04:42 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://linktree.cn/hukai' },
+    { title: '发现周边，特惠同城吃喝玩乐', url: 'http://h.fxzb.vip/1UiUJ1CF76U' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '我杂乱无章的记事本',
+  HEO_HERO_TITLE_2: '分享学习记录和生活',
+  HEO_HERO_TITLE_3: 'HUKAI.FUN',
+  HEO_HERO_TITLE_4: '欢迎光临',
+  HEO_HERO_TITLE_5: '个人微信公众号：JavaGym',
+  HEO_HERO_TITLE_LINK: 'https://linktree.cn/hukai',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
