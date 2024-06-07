bac58fcba8a41321c01edbb510b1f018bd2d5fb9
胡凯 <46771180+Kaykun@users.noreply.github.com>
2024-06-07 21:40:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -105,10 +105,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '微信公众号',
+  HEO_SOCIAL_CARD_TITLE_2: '关注公众号获取更多资讯',
+  HEO_SOCIAL_CARD_TITLE_3: '点击跳转微信',
+  HEO_SOCIAL_CARD_URL: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI0MjE0MTMxMw==&scene=124#wechat_redirect',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
